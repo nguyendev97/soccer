fa388c348a92977667695f9cd65edaf194bf43ab
huquduy <huquduy@gmail.com>
2022-10-28 17:46:35 +0700

    bugfixes: gas price

--- src/views/Marketplace/components/OpenBoxesModal.tsx
+++ src/views/Marketplace/components/OpenBoxesModal.tsx
@@ -105,7 +105,7 @@ const OpenBoxesModal: React.FC<React.PropsWithChildren<OpenBoxesModalProps>> = (
       )
     },
     onConfirm: () => {
-      return callWithEstimateGas(selectedBoxesOpenContract, 'open', [Date.now(), [type === HALLOWEEN ? HALLOWEEN_TYPE : SPECIAL_TYPE], [amountBoxes]], null, 10000)
+      return callWithEstimateGas(selectedBoxesOpenContract, 'open', [Date.now(), [type === HALLOWEEN ? HALLOWEEN_TYPE : SPECIAL_TYPE], [amountBoxes]], null, 500000)
     },
     onSuccess: async ({ receipt }) => {
       playersContract
