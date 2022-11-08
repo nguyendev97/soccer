5efa51832ddc317b04d1a3ac2b2556fa9ab1daee
huquduy <huquduy@gmail.com>
2022-11-09 02:35:31 +0700

    fixes: update  images

---
+++ src/views/Nft/market/Collection/IndividualNFTPage/OneOfAKindNftPage/images/index.ts
@@ -0,0 +1,12 @@
+import powImage from './pow.png'
+import shoImage from './sho.png'
+import speImage from './spe.png'
+import jmpImage from './jmp.png'
+
+
+export {
+  powImage,
+  shoImage,
+  speImage,
+  jmpImage,
+}
