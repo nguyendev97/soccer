c97bbcfc797656d718d71c25143d6422a173f38c
kakarot <example@gmail.com>
2022-11-18 15:11:19 +0700

    Up

--- src/views/Marketplace/DailyQuest/index.tsx
+++ src/views/Marketplace/DailyQuest/index.tsx
@@ -46,9 +46,6 @@ const CheckInSection = () => {
             <Day>{`Day ${position}`}</Day>
           </DayWrapper>
         ))}
-        {/* Example */}
-        {/* Timeline position 0/3 = 0%; 1/3 = 33%; 2/3 = 66%; 3/3 = 100% */}
-        {/* <Timeline position={1 / 3} /> */}
       </TimelineWrapper>
     </SectionWrapper>
   )
@@ -101,11 +98,10 @@ const QuestSection = () => {
       </DeadlineWrapper>
 
       <LineTitle>Quest Reward</LineTitle>
-
-      <QuestWrapper>
+      <QuestReward>
         <p style={{ marginBottom: 24 }}>Quest Stage</p>
         <TimelineWrapper style={{ marginTop: 60 }}>
-          <Timeline position={0 / (pointArray.length - 1)} />
+          <Timeline position={1 / 4} />
           {questArray.map(({ position, active, reward }) => (
             <DayWrapper position={position / 4} key={position}>
               {reward && (
@@ -119,14 +115,40 @@ const QuestSection = () => {
             </DayWrapper>
           ))}
         </TimelineWrapper>
-      </QuestWrapper>
-      <LineTitle>Quest Stage # 1</LineTitle>
+      </QuestReward>
+      <LineTitle>
+        Quest Stage <span style={{ color: '#36DBFF' }}># 1</span>
+      </LineTitle>
+
+      {/* Carousel here */}
+      <QuestStage>
+        <QuestItem icon="/icons/playlist.svg" title="Watch video" desc="Complete quests to earn quest points" />
+        <QuestItem icon="/icons/facebook.svg" title="Follow fanpage" desc="Complete quests to earn quest points" />
+      </QuestStage>
     </SectionWrapper>
   )
 }
 
+const QuestItem = ({ icon, title, desc }) => {
+  return (
+    <QuestWrapper>
+      <QuestImageWrapper>
+        <img src={icon} alt="" />
+      </QuestImageWrapper>
+      <QuestContent>
+        <h4>{title}</h4>
+        <p>{desc}</p>
+        <RangeInputWrapper>
+          <RangeInput type="range" min={0} max={1} />
+          0/1
+        </RangeInputWrapper>
+      </QuestContent>
+    </QuestWrapper>
+  )
+}
+
 const questArray = [
-  { position: 1, active: false, reward: 843.56 },
+  { position: 1, active: true, reward: 843.56 },
   { position: 2, active: false, reward: 843.56 },
   { position: 3, active: false, reward: 843.56 },
 ]
@@ -244,21 +266,27 @@ const GradientBox = styled.div`
 const TimelineWrapper = styled.div`
   position: relative;
   width: 348px;
-  height: 0px;
-  border: 0.5px solid #9197ba;
+  height: 6px;
+  background: #0e0049;
+  border-radius: 10px;
   margin: 30px 0;
+  @media (max-width: 576px) {
+    width: 290px;
+  }
 `
 
 const Timeline = styled.div<{ position: number }>`
   position: absolute;
-  top: -1px;
+  top: 0;
   left: 0;
-  border: 1px solid #36dbff;
+  height: 6px;
+  background: #36dbff;
+  border-radius: 10px;
   width: calc(${({ position }) => position * 100}%);
 `
 const DayWrapper = styled.div<{ position: number }>`
   position: absolute;
-  top: -4px;
+  top: -1px;
   left: calc(${({ position }) => position * 100}% - 4px);
 `
 const Day = styled.p`
@@ -315,6 +343,7 @@ const LineTitle = styled.div`
   font-size: 18px;
   line-height: 160%;
   color: #ffffff;
+  margin: 40px 0 30px;
 
   ::before,
   ::after {
@@ -324,6 +353,10 @@ const LineTitle = styled.div`
     width: 184.5px;
     height: 0px;
     border-top: 0.5px solid #9197ba;
+
+    @media (max-width: 576px) {
+      width: 80px;
+    }
   }
   ::before {
     right: calc(100% + 20px);
@@ -332,8 +365,71 @@ const LineTitle = styled.div`
     left: calc(100% + 20px);
   }
 `
-const QuestWrapper = styled.div`
+const QuestReward = styled.div`
   display: flex;
   align-items: flex-end;
   gap: 20px;
+  @media (max-width: 576px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`
+const QuestStage = styled.div`
+  display: flex;
+  gap: 30px;
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+  }
+`
+
+const QuestWrapper = styled.div`
+  display: flex;
+  padding: 10px 30px 10px 10px;
+  gap: 12px;
+  /* width: 407px; */
+  height: 110px;
+  background: #1d018d;
+  border-radius: 10px;
+`
+const QuestImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  width: 90px;
+  height: 90px;
+  background: linear-gradient(205.65deg, #263192 2.69%, #1d096b 47.14%, #0046cd 93.07%);
+  border-radius: 10px;
+`
+const QuestContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  gap: 4px;
+
+  h4 {
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 160%;
+    color: #ffffff;
+  }
+  p {
+    font-size: 12px;
+    line-height: 160%;
+    color: #9197ba;
+  }
+`
+
+const RangeInputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+const RangeInput = styled.input`
+  &[type='range'] {
+    height: 6px;
+    border-radius: 10px;
+    background: #130355;
+  }
 `
