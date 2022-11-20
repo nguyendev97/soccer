590985b04edeec77c4a877bb38f8246a181d0a9a
huquduy <huquduy@gmail.com>
2022-11-21 00:37:06 +0700

    fixes: inventory

--- src/views/Marketplace/SoccerBox/index.tsx
+++ src/views/Marketplace/SoccerBox/index.tsx
@@ -18,10 +18,16 @@ export const Container = styled.div`
 
 const SPECIAL_TYPE = 1
 const HALLOWEEN_TYPE = 5
+const COMMON_TYPE = 4
+const GOLDEN_TYPE = 2
+const SILVER_TYPE = 3
 
 const SoccerBox = () => {
   const { account, chainId } = useWeb3React()
   const [amountBox, setAmountBox] = useState(0)
+  const [amountGoldenBox, setAmountGoldenBox] = useState(0)
+  const [amountSilverBox, setAmountSilverBox] = useState(0)
+  const [amountCommonBox, setAmountCommonBox] = useState(0)
   const [amountHalloweenBox, setAmountHalloweenBox] = useState(0)
   const boxesAddress = getBoxesAddress(chainId)
   const boxesContract = useERC1155(boxesAddress)
@@ -35,6 +41,15 @@ const SoccerBox = () => {
       boxesContract.balanceOf(account, HALLOWEEN_TYPE).then((res) => {
         setAmountHalloweenBox(res.toNumber())
       })
+      boxesContract.balanceOf(account, GOLDEN_TYPE).then((res) => {
+        setAmountGoldenBox(res.toNumber())
+      })
+      boxesContract.balanceOf(account, SILVER_TYPE).then((res) => {
+        setAmountSilverBox(res.toNumber())
+      })
+      boxesContract.balanceOf(account, COMMON_TYPE).then((res) => {
+        setAmountCommonBox(res.toNumber())
+      })
     }
   }, [account, boxesContract, currentBlock])
 
@@ -45,7 +60,7 @@ const SoccerBox = () => {
     <>
       <StyledFlexWrapper>
         <Container>
-          <Grid gridTemplateColumns={["repeat(2, 1fr)", null, "repeat(3, 1fr)"]} gridColumnGap="16px" gridRowGap="8px">
+          <Grid gridTemplateColumns={["repeat(2, 1fr)", null, "repeat(3, 1fr)"]} gridColumnGap="16px" gridRowGap="16px">
             <BoxItem
               disabled={amountHalloweenBox < 1}
               totalBox={amountHalloweenBox}
@@ -59,6 +74,27 @@ const SoccerBox = () => {
               avatar="/videos/special.mp4"
               boxName="Special box"
               onClick={onPresentOpenBoxesModal}
+            />
+            <BoxItem
+              disabled={amountGoldenBox < 1}
+              totalBox={amountGoldenBox}
+              avatar="/videos/Golden.mp4"
+              boxName="Golden box"
+              onClick={null}
+            />
+            <BoxItem
+              disabled={amountSilverBox < 1}
+              totalBox={amountSilverBox}
+              avatar="/videos/Silver.mp4"
+              boxName="Silver box"
+              onClick={null}
+            />
+            <BoxItem
+              disabled={amountCommonBox < 1}
+              totalBox={amountCommonBox}
+              avatar="/videos/common.mp4"
+              boxName="Common box"
+              onClick={null}
             />
           </Grid>
         </Container>
