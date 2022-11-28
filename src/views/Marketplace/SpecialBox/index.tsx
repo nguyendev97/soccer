4b82a48000b46551d73e49f1e906ebef722a9ace
huquduy <huquduy@gmail.com>
2022-11-28 23:33:36 +0700

    fixes: disabled buying

--- src/views/Marketplace/SpecialBox/index.tsx
+++ src/views/Marketplace/SpecialBox/index.tsx
@@ -153,7 +153,7 @@ const SpecialBox = () => {
                   (<Flex flexDirection="column">
                     <GradientButton
                       endIcon={isApproving || isConfirming ? <AutoRenewIcon spin color="currentColor" /> : undefined}
-                      disabled
+                      disabled={isApproving || isConfirming || isNotEnoughBalance}
                       onClick={isApproved ? handleConfirm : handleApprove}
                       fontSize="16px"
                       fontWeight="700"
